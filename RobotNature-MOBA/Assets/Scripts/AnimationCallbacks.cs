084a87d5d7190274ff0ca2c68cdc66a0751ceb4d
Théo <t.marcuzzi@rubika-edu.com>
2022-12-20 15:22:00 +0100

    Fix Animation

--- RobotNature-MOBA/Assets/Scripts/AnimationCallbacks.cs
+++ RobotNature-MOBA/Assets/Scripts/AnimationCallbacks.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class AnimationCallbacks : MonoBehaviour
+public class AnimationCallbacks : MonoBehaviourPun
 {
     [SerializeField] private Transform castTransform;
     public ICastable caster;
@@ -11,11 +9,24 @@ public class AnimationCallbacks : MonoBehaviour
     
     public void AnimationCast()
     {
+        if(photonView.IsMine)photonView.RPC("AnimationCastRPC", RpcTarget.MasterClient);
+    }
+
+    [PunRPC]
+    void AnimationCastRPC()
+    {
         caster.CastAnimationCast(castTransform);
     }
 
     public void AnimationEnd()
     {
+        if(photonView.IsMine)photonView.RPC("AnimationEndRPC", RpcTarget.MasterClient);
+    }
+
+    [PunRPC]
+    public void AnimationEndRPC()
+    {
         caster.CastAnimationEnd();
     }
+    
 }
