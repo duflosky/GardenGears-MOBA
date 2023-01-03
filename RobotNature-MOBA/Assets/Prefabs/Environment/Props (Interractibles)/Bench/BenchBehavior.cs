586ca178ea184b417fe177a8da0d4837617963fd
Maxime Duflot <maximedft@outlook.com>
2023-01-03 16:01:36 +0100

    Fix Bench

--- RobotNature-MOBA/Assets/Prefabs/Environment/Props (Interractibles)/Bench/BenchBehavior.cs
+++ RobotNature-MOBA/Assets/Prefabs/Environment/Props (Interractibles)/Bench/BenchBehavior.cs
@@ -1,27 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using Entities;
 using UnityEngine;
 
 public class BenchBehavior : MonoBehaviour
 {
-    public Animation An_bench;
-    public Collider BenchCollider;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Collider benchCollider;
 
     private void OnTriggerEnter(Collider other)
     {
-        An_bench.Play();
-        BenchCollider.enabled = false;
+        if (other.GetComponent<Entity>() == null) return;
+        GetComponent<Animator>().SetTrigger("isBroken");
+        benchCollider.enabled = false;
     }
 }
